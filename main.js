898036cce7c31dc2a6234136fcc0bc8776ebac02
Sidali04 <zerarisidaliofficiel74@gmail.com>
2024-09-14 18:26:28 +0100

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,104 @@
+/*this function will be return element get it by id selector */
 let getId = (id)=>{
     return document.getElementById(id);
 }
+getId('Alaa_logo').addEventListener('click',()=>{
+    scrollTo({
+        top:0,
+        behavior:"smooth",
+    });
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true
+    getId('home').style.display = 'block';
+    getId('architecture').style.display = 'none';
+    getId('interior').style.display = 'none';
+    getId('product').style.display = 'none';
+    getId('team').style.display = 'none';
+    getId('gallery').style.display = 'none';
+    getId('contact').style.display = 'none';
+});
+/*this function will be do when i click at button display the special div of this button*/
+getId('HOME_btn').addEventListener('click',()=>{
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true
+    getId('home').style.display = 'block';
+    getId('architecture').style.display = 'none';
+    getId('interior').style.display = 'none';
+    getId('product').style.display = 'none';
+    getId('team').style.display = 'none';
+    getId('gallery').style.display = 'none';
+    getId('contact').style.display = 'none';
+});
+getId('ARCHITECTURE_btn').addEventListener('click',()=>{
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true;
+    getId('home').style.display = 'none';
+    getId('architecture').style.display = 'block';
+    getId('interior').style.display = 'none';
+    getId('product').style.display = 'none';
+    getId('team').style.display = 'none';
+    getId('gallery').style.display = 'none';
+    getId('contact').style.display = 'none';
+});
+getId('INTERIOR_btn').addEventListener('click',()=>{
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true;
+    getId('home').style.display = 'none';
+    getId('architecture').style.display = 'none';
+    getId('interior').style.display = 'block';
+    getId('product').style.display = 'none';
+    getId('team').style.display = 'none';
+    getId('gallery').style.display = 'none';
+    getId('contact').style.display = 'none';
+});
+getId('PRODUCTS_btn').addEventListener('click',()=>{
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true;
+    getId('home').style.display = 'none';
+    getId('architecture').style.display = 'none';
+    getId('interior').style.display = 'none';
+    getId('product').style.display = 'block';
+    getId('team').style.display = 'none';
+    getId('gallery').style.display = 'none';
+    getId('contact').style.display = 'none';
+});
+getId('TEAM_btn').addEventListener('click',()=>{
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true;
+    getId('home').style.display = 'none';
+    getId('architecture').style.display = 'none';
+    getId('interior').style.display = 'none';
+    getId('product').style.display = 'none';
+    getId('team').style.display = 'block';
+    getId('gallery').style.display = 'none';
+    getId('contact').style.display = 'none';
+});
+getId('GALLERY_btn').addEventListener('click',()=>{
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true;
+    getId('home').style.display = 'none';
+    getId('architecture').style.display = 'none';
+    getId('interior').style.display = 'none';
+    getId('product').style.display = 'none';
+    getId('team').style.display = 'none';
+    getId('gallery').style.display = 'block';
+    getId('contact').style.display = 'none';
+});
+getId('CONTACT_btn').addEventListener('click',()=>{
+    document.querySelector('nav').style.height = '0vh';
+    hide_show_nav = true;
+    getId('home').style.display = 'none';
+    getId('architecture').style.display = 'none';
+    getId('interior').style.display = 'none';
+    getId('product').style.display = 'none';
+    getId('team').style.display = 'none';
+    getId('gallery').style.display = 'none';
+    getId('contact').style.display = 'block';
+});
 let hide_show_nav = true;
 document.querySelector('nav').style.transition = '.4vh';
+/*this function do when you click at dirst time this button wil be show options div
+and in the second time will be hide the options div */
 getId('btn_list').addEventListener('click',()=>{
     if (hide_show_nav) {
         document.querySelector('nav').style.height = '54vh'; 
@@ -12,6 +108,7 @@ getId('btn_list').addEventListener('click',()=>{
         hide_show_nav = true;
     }
 });
+/*this function change the main home img */
 let change_main_img = true;
 setInterval(()=>{
     if (change_main_img) {
@@ -22,6 +119,18 @@ setInterval(()=>{
         change_main_img = true;
     }
 },5000);
+/*this function change the main interior img */
+let change_main_interior_img = true;
+setInterval(()=>{
+    if (change_main_interior_img) {
+        getId('div_main_img_interior').style.backgroundImage = 'url(main_img_interior2.png)';
+        change_main_interior_img = false;
+    }else{
+        getId('div_main_img_interior').style.backgroundImage = 'url(main_img_interior1.png)';
+        change_main_interior_img = true;
+    }
+},5000);
+/* this function do when you click in ine of this button will be show a message*/
 getId('WE_CREATE_NEW').addEventListener('click',()=>{
     getId('add_paragraph_to_pre').innerHTML = 
     `New We don’t draw designs and sketches.
@@ -71,4 +180,19 @@ getId('WE_DELIVER').addEventListener('click',()=>{
     qualifies for Hatch Selection, it meets the most uncompromising 
     standards for your pleasure.
     `;
+});
+/*if the scroll is greatest then 1500px the website will be show the up button*/
+window.addEventListener('scroll',()=>{
+    if(scrollY > 1500){
+        getId('up').style.display = 'block';
+    }else{
+        getId('up').style.display = 'none';
+    }
+});
+/* when you click at up button the site web will be scroll to header div*/
+getId('up').addEventListener('click',()=>{
+    scrollTo({
+        top:0,
+        behavior:'smooth',
+    })
 });
